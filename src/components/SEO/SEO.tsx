caa5ab0301464897b6787d76bca6da8d2c897fb8
Vim USDS <vimal.k.shah@omb.eop.gov>
2023-01-03 16:26:22 -0800

    fix: update page title w/ i18n
    - Head API cannot support i18m
    - Replace Head API with react-helmet
    - closes #5

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 // import * as styles from './SEO.module.scss';
 
 export interface ISEOProps {
   children?: React.ReactNode;
-  title: string;
+  title: React.ReactNode;
 }
 
 const SEO = ({ title }: ISEOProps) => {
-  // This SEO boilerplate was inspired by https://gist.github.com/nunosans/3028849
+  // The reason why Gatsby HEAD API is not being used is because of:
+  // https://github.com/gatsbyjs/gatsby/issues/36458#issuecomment-1334232043
 
   return (
-    <>
-      {/* Priority tags. These must come first. */}
-      <meta charSet="utf-8" />
+    <Helmet defer={false}>
+      <title>{`${title}`}</title>
 
-      {/* Render Chrome if available or using latest version of Internet Explorer (Recommended). */}
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge; chrome=1" />
+      {/* Description content should ideally be between 160 - 200 characters */}
+      <meta
+        name="description"
+        content={`This new web app will do X for the government providing Y, Z and W.`}
+      />
 
-      {/* Document Title */}
-      <title>{title}</title>
+      {/* Keyword content should list the top 10 words in order of relevance. The words chosen need
+        to appear on every page. These words were chosen from the header as it appears on each page */}
+      <meta
+        name="keywords"
+        content={`screening tool climate environmental economic justice beta council quality methodology`}
+      />
 
-      {/* Open Graph Meta Tags (ogp.me) */}
-      <meta property="og:site_name" content="Site Name" />
-      <meta property="og:title" content="Page Title" />
-      <meta property="og:description" content="Description of this web page" />
-    </>
+      {/* Allows for Open Graph meta tags */}
+      <meta property="og:url" content="https://new-usds-app.gov" />
+      <meta property="og:title" content="New USDS App" />
+      <meta property="og:site_name" content="New USDS App" />
+      <meta
+        property="og:description"
+        content={`This new web app will do X for the government providing Y, Z and W.`}
+      />
+
+      {/* DAP Tag */}
+      <script></script>
+    </Helmet>
   );
 };
 
