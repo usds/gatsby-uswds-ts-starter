2558e9575ae6973699a7e5fec451cca56889dc19
Vim USDS <vimal.k.shah@omb.eop.gov>
2023-01-05 14:45:50 -0800

    fix: add gh-pages deploy script

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,6 +14,7 @@ require(`dotenv`).config({
 });
 
 const config: GatsbyConfig = {
+  pathPrefix: process.env.DEPLOY_PREFIX || ``,
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   plugins: [
